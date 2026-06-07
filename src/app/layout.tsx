1b0b0c71a1fe08dc92dbe4f97e3af4ceb3dd6286
kapmadusanka <kapmadusanka95@gmail.com>
2026-06-07 13:20:35 +0530

    add Microsoft Clarity integration to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -257,6 +257,17 @@ export default function RootLayout({
             gtag('config', 'G-7GP5GV6ECQ');
           `}
         </Script>
+
+        {/* Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "x34ffk76cq");
+          `}
+        </Script>
       </body>
     </html>
   )
